33c9b8feb62878be3cdac4d38301aefe320fd642
Sameer Ramesh biradar <biradarsameer2@gmail.com>
2024-09-09 13:44:29 +0530

    threds in  -> { };

---
+++ java8/LambdaExpressions/ThredsInLambda.java
@@ -0,0 +1,39 @@
+package java8.LambdaExpressions;
+
+public class ThredsInLambda {
+    public static void main(String[] args) {
+
+        Runnable sameer = () -> {
+            Thread.currentThread().setName("sameer");
+            System.out.println(Thread.currentThread().getName()+" is running");
+
+            long number = 2; // Start from the first prime number
+            while (true) {
+                if (isPrime(number)) {
+                    System.out.println(number + " is a prime number");
+                }
+                number++;
+            }
+        };
+
+
+
+        Thread a =new Thread(sameer);
+
+
+        a.start();
+    }
+    public static boolean isPrime(long num) {
+        if (num <= 1) return false;
+        if (num == 2 || num == 3) return true; // 2 and 3 are prime numbers
+        if (num % 2 == 0 || num % 3 == 0) return false; // Eliminate multiples of 2 and 3
+
+        // Check for factors starting from 5
+        for (long i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
